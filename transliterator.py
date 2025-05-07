29d9024c3158d923aab15cdc96b0010cec343ae5
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-05-07 16:06:16 +0530

    added slp1 to devanagari transliteration

--- transliterator.py
+++ transliterator.py
@@ -4,5 +4,8 @@ from indic_transliteration.sanscript import transliterate
 def DEVANAGARI_to_SLP1(text):
     return transliterate(text, sanscript.DEVANAGARI, sanscript.SLP1)
 
+def SLP1_to_DEVANAGARI(text):
+    return transliterate(text, sanscript.SLP1, sanscript.DEVANAGARI)
+
 def ITRANS_to_DEVANAGARI(text):
     return transliterate(text, sanscript.ITRANS, sanscript.DEVANAGARI)
